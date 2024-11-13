e2d2591c21189d434a90682c0b1339096c8552a2
黄紫枫 <873511650@qq.com>
2024-11-13 21:49:43 +0800

    feat: table

--- examples/router/index.js
+++ examples/router/index.js
@@ -4,7 +4,7 @@ import Home from '../views/index.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
+export const routes = [
   {
     path: '/',
     name: 'Home',
@@ -13,42 +13,66 @@ const routes = [
   {
     path: '/cascader',
     name: 'cascader',
-    component: () => import('../views/cascader')
+    component: () => import('../views/cascader'),
+    meta: {
+      title: 'cascader 多选级联'
+    }
   },
   {
     path: '/table',
     name: 'table',
-    component: () => import('../views/table')
+    component: () => import('../views/table'),
+    meta: {
+      title: 'table'
+    }
   },
   {
     path: '/textarea',
     name: 'textarea',
-    component: () => import('../views/textarea')
+    component: () => import('../views/textarea'),
+    meta: {
+      title: 'textarea 文本域'
+    }
   },
   {
     path: '/input',
     name: 'input',
-    component: () => import('../views/input')
+    component: () => import('../views/input'),
+    meta: {
+      title: 'input 文本'
+    }
   },
   {
     path: '/input-number',
     name: 'input-number',
-    component: () => import('../views/input-number')
+    component: () => import('../views/input-number'),
+    meta: {
+      title: 'input-number 文本数字'
+    }
   },
   {
     path: '/select',
     name: 'select',
-    component: () => import('../views/select')
+    component: () => import('../views/select'),
+    meta: {
+      title: 'select 选择框'
+    }
   },
   {
     path: '/multiple-select',
-    name: 'select',
-    component: () => import('../views/multiple-select')
+    name: 'multiple-select',
+    component: () => import('../views/multiple-select'),
+    meta: {
+      title: 'multiple-select 多个下拉框'
+    }
   },
   {
     path: '/date-picker',
     name: 'date-picker',
-    component: () => import('../views/date-picker')
+    component: () => import('../views/date-picker'),
+    meta: {
+      title: 'date-picker 日期选择器'
+    }
   }
 ]
 
--- examples/views/index.vue
+++ examples/views/index.vue
@@ -1,152 +1,45 @@
 <template>
-  <div style="width: 300px">
-    <a-table
-      :columns="columns"
-      :data-source="data"
-      :scroll="{ x: 500 }"
-      v-insert-content="customContent"
-    >
-      <a slot="name" slot-scope="text">{{ text }}</a>
-      <span slot="customTitle"><a-icon type="smile-o" /> Name</span>
-      <span slot="tags" slot-scope="tags">
-        <a-tag
-          v-for="tag in tags"
-          :key="tag"
-          :color="
-            tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'
-          "
-        >
-          {{ tag.toUpperCase() }}
-        </a-tag>
-      </span>
-      <span slot="action" slot-scope="text, record">
-        <a>Invite 一 {{ record.name }}</a>
-        <a-divider type="vertical" />
-        <a>Delete</a>
-        <a-divider type="vertical" />
-        <a class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
-      </span>
-    </a-table>
-    <a-table :columns="columns" :data-source="data">
-      <a slot="name" slot-scope="text">{{ text }}</a>
-      <span slot="customTitle"><a-icon type="smile-o" /> Name</span>
-      <span slot="tags" slot-scope="tags">
-        <a-tag
-          v-for="tag in tags"
-          :key="tag"
-          :color="
-            tag === 'loser' ? 'volcano' : tag.length > 5 ? 'geekblue' : 'green'
-          "
-        >
-          {{ tag.toUpperCase() }}
-        </a-tag>
-      </span>
-      <span slot="action" slot-scope="text, record">
-        <a>Invite 一 {{ record.name }}</a>
-        <a-divider type="vertical" />
-        <a>Delete</a>
-        <a-divider type="vertical" />
-        <a class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
-      </span>
-    </a-table>
+  <div class="menu-main">
+    <div v-for="item in arr.filter(v=>v.meta)" :key="item.name" class="item-box" @click="itemClick(item)">
+      {{item.meta.title}}
+    </div>
   </div>
 </template>
 <script>
-const columns = [
-  {
-    dataIndex: 'name',
-    key: 'name',
-    slots: { title: 'customTitle' },
-    scopedSlots: { customRender: 'name' }
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age'
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address'
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    scopedSlots: { customRender: 'tags' }
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    scopedSlots: { customRender: 'action' }
-  }
-]
-
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer']
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser']
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher']
-  }
-]
-
+import { routes } from '../router/index'
 export default {
-  directives: {
-    insertContent: {
-      bind (el, binding) {
-        if (binding.value) {
-          const targetDiv = el.querySelector('.ant-table-scroll') // 确保选择正确的元素
-          const chidDiv = targetDiv.querySelector('.d-table-sticky-scroll') // 确保选择正确的元素
-          if (targetDiv && !chidDiv) {
-            const newDiv = document.createElement('div')
-            newDiv.className = 'd-table-sticky-scroll'
-            const innerDiv = document.createElement('div')
-            innerDiv.className = 'd-table-sticky-scrol-bar' // 可以设置内部 div 的类名或者其他属性
-
-            // 将内部 div 插入到新的 div 中
-            newDiv.appendChild(innerDiv)
-
-            // 将新的 div 元素插入到目标 div 中
-            targetDiv.appendChild(newDiv)
-          }
-        }
-      }
-    }
-  },
   data () {
     return {
-      data,
-      columns,
-      customContent: '<div>这是自定义内容</div>' // 要插入的内容
+      arr: routes
+    }
+  },
+  methods: {
+    itemClick (item) {
+      this.$router.push({
+        path: item.path
+      })
     }
   }
 }
 </script>
-<style lang="less">
-.d-table-sticky-scroll{
-  width: 100%;
-  height: 5px;
-  background: red;
-  .d-table-sticky-scrol-bar{
-    height: 100%;
-    width: 50%;
-    background: yellow;
+<style lang="less" scoped>
+.menu-main{
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  .item-box{
+    width: 200px;
+    height: 100px;
+    cursor: pointer;
+    border-radius: 4px;
+    border: 1px solid #444;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:hover{
+      box-shadow: 2px 4px 5px #666;
+    }
   }
 }
+
 </style>
