f9135e4bf94ed6e63c64bb9bb2d21b0e3de828e4
黄紫枫 <873511650@qq.com>
2024-11-20 16:36:12 +0800

    feat: 例子

--- examples/main.js
+++ examples/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import dConfig from './utils/dConfig'
 import DesignUI from '../packages'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.less'
@@ -9,7 +9,7 @@ import 'ant-design-vue/dist/antd.less'
 Vue.use(Antd)
 Vue.use(DesignUI)
 Vue.config.productionTip = false
-
+DesignUI.config.$dConfig = dConfig
 new Vue({
   router,
   render: h => h(App)
