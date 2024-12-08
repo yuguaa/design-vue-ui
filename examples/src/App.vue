50fb4501e56352939b11a2b35c02691a36407d35
黄紫枫 <873511650@qq.com>
2024-12-08 10:04:34 +0800

    feat: 例子

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
-</template>
+  <a-config-provider :locale="locale">
+    <div id="app">
+      <router-view />
+    </div>
+  </a-config-provider>
 
+</template>
+<script>
+import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
+export default {
+  data() {
+    return {
+      locale: zhCN,
+    }
+  },
+}
+</script>
 <style lang="less">
 .xm-main {
   padding: 48px;
+
   h2 {
     margin: 24px 0;
   }
